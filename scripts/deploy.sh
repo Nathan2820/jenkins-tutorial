98c212ff2659f46b7436bd3d5885ee1eb0fe6813
Nathan Orsmond <59610591+Nathan2820@users.noreply.github.com>
2020-09-17 15:34:07 +0100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,3 @@
 #! /bin/bash
 cd chaperootodo_client
-
-sudo chown jenkins /var/run/docker.sock
-sudo chmod 666 /var/run/docker.sock
-
-docker-compose up -d
+sudo docker-compose pull && sudo -E DBPASSWORD={DB_PASSWORD} && sudo docker-compose up -d
